bc6bde39c4829a4d1a10ebd48e07e364921f2674
Your NameSina <sinaazadfar@gmail.com>
2023-09-16 17:21:29 +0330

    sina-productComment

--- components/section/HeaderHero.vue
+++ components/section/HeaderHero.vue
@@ -7,16 +7,14 @@
             </div>
         </div>
 
-        <div class="md:col-span-1 col-span-2   h-[36rem]">
-            <div class="w-full bg-gray-200 pb-1 rounded-xl h-[18rem]">
-                <img src="/images/header2.webp" class="h-full rounded-xl w-full object-cover cover" alt="">
-
-            </div>
-            <div class="w-full bg-gray-200 pt-1 rounded-xl h-[18rem]">
-                <img src="/images/header3.jpeg" class="h-full rounded-xl w-full object-cover cover" alt="">
-
-            </div>
-        </div>
+        <div class="hidden md:block md:col-span-1 col-span-2 h-[36rem]">
+    <div class="w-full bg-gray-200 pb-1 rounded-xl h-[18rem]">
+        <img src="/images/header2.webp" class="h-full rounded-xl w-full object-cover cover" alt="">
+    </div>
+    <div class="w-full bg-gray-200 pt-1 rounded-xl h-[18rem]">
+        <img src="/images/header3.jpeg" class="h-full rounded-xl w-full object-cover cover" alt="">
+    </div>
+</div>
 
     </div>
 </template>
