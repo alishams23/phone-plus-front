20952a59c899521531499875d34e2d373c6dc3e1
ali <alishams.s1380@gmail.com>
2023-09-16 14:48:07 +0330

    f

--- pages/product/[product].vue
+++ pages/product/[product].vue
@@ -14,181 +14,269 @@
   ```
 -->
 <template>
-    <div class="bg-white">
-      <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <div class="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-          <!-- Image gallery -->
-          <TabGroup as="div" class="flex flex-col-reverse">
-            <!-- Image selector -->
-            <div class="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-              <TabList class="grid grid-cols-4 gap-6">
-                <Tab v-for="image in product.images" :key="image.id" class="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4" v-slot="{ selected }">
-                  <span class="sr-only">{{ image.name }}</span>
-                  <span class="absolute inset-0 overflow-hidden rounded-md flex justify-center">
-                    <img :src="image.src" alt="" class="h-full  object-center" />
-                  </span>
-                  <span :class="[selected ? 'ring-indigo-500' : 'ring-transparent', 'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2']" aria-hidden="true" />
-                </Tab>
-              </TabList>
-            </div>
-  
-            <TabPanels class="aspect-h-1 aspect-w-1 w-full">
-              <TabPanel v-for="image in product.images" :key="image.id" class="h-[500px]" >
-                <img :src="image.src" :alt="image.alt" class="h-full object-center sm:rounded-lg" />
-              </TabPanel>
-
-            </TabPanels>
-          </TabGroup>
-  
-          <!-- Product info -->
-          <div class="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0 rtl">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ product.name }}</h1>
-  
-            <div class="mt-3">
-              <h2 class="sr-only">Product information</h2>
-              <p class="text-3xl tracking-tight text-gray-900">{{ product.price }}</p>
-            </div>
-  
-            <!-- Reviews -->
-            <div class="mt-3">
-              <h3 class="sr-only">Reviews</h3>
-              <div class="flex items-center">
-                <div class="flex items-center">
-                  <StarIcon v-for="rating in [0, 1, 2, 3, 4]" :key="rating" :class="[product.rating > rating ? 'text-indigo-500' : 'text-gray-300', 'h-5 w-5 flex-shrink-0']" aria-hidden="true" />
-                </div>
-                <p class="sr-only">{{ product.rating }} out of 5 stars</p>
-              </div>
-            </div>
-  
-            <div class="mt-6">
-              <h3 class="sr-only">Description</h3>
-  
-              <div class="space-y-6 text-base text-gray-700" v-html="product.description" />
-            </div>
-  
-            <form class="mt-6">
-              <!-- Colors -->
-              <div class="ltr flex items-end flex-col">
-                <h3 class="text-sm text-gray-600 rtl">رنگ ها </h3>
-  
-                <RadioGroup v-model="selectedColor" class="mt-2">
-                  <RadioGroupLabel class="sr-only">Choose a color</RadioGroupLabel>
-                  <span class="flex items-center space-x-3">
-                    <RadioGroupOption as="template" v-for="color in product.colors" :key="color.name" :value="color" v-slot="{ active, checked }">
-                      <div :class="[color.selectedColor, active && checked ? 'ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', 'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none']">
-                        <RadioGroupLabel as="span" class="sr-only">{{ color.name }}</RadioGroupLabel>
-                        <span aria-hidden="true" :class="[color.bgColor, 'h-8 w-8 rounded-full border border-black border-opacity-10']" />
-                      </div>
-                    </RadioGroupOption>
-                  </span>
-                </RadioGroup>
-              </div>
-  
-              <div class="sm:flex-col1 mt-10 flex">
-                <button type="submit" class="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full">اضافه کردن به سبد خرید</button>
-  
-                <button type="button" class="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
-                  <HeartIcon class="h-6 w-6 flex-shrink-0" aria-hidden="true" />
-                  <span class="sr-only">اضافه کردن به علاقه کندی ها</span>
-                </button>
-              </div>
-            </form>
-  
-            <section aria-labelledby="details-heading" class="mt-12">
-              <h2 id="details-heading" class="sr-only">Additional details</h2>
-  
-              <div class="divide-y divide-gray-200 border-t">
-                <Disclosure as="div" v-for="detail in product.details" :key="detail.name" v-slot="{ open }">
-                  <h3>
-                    <DisclosureButton class="group relative flex w-full items-center justify-between py-6 text-left">
-                      <span :class="[open ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-medium']">{{ detail.name }}</span>
-                      <span class="ml-6 flex items-center">
-                        <PlusIcon v-if="!open" class="block h-6 w-6 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-                        <MinusIcon v-else class="block h-6 w-6 text-indigo-400 group-hover:text-indigo-500" aria-hidden="true" />
-                      </span>
-                    </DisclosureButton>
-                  </h3>
-                  <DisclosurePanel as="div" class="prose prose-sm pb-6">
-                    <ul role="list">
-                      <li v-for="item in detail.items" :key="item">{{ item }}</li>
-                    </ul>
-                  </DisclosurePanel>
-                </Disclosure>
-              </div>
-            </section>
+  <div class="bg-white">
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div class="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+        <!-- Image gallery -->
+        <TabGroup as="div" class="flex flex-col-reverse">
+          <!-- Image selector -->
+          <div class="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+            <TabList class="grid grid-cols-4 gap-6">
+              <Tab v-for="image in product.images" :key="image.id"
+                class="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+                v-slot="{ selected }">
+                <span class="sr-only">{{ image.name }}</span>
+                <span class="absolute inset-0 overflow-hidden rounded-md flex justify-center">
+                  <img :src="image.src" alt="" class="h-full  object-center" />
+                </span>
+                <span
+                  :class="[selected ? 'ring-indigo-500' : 'ring-transparent', 'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2']"
+                  aria-hidden="true" />
+              </Tab>
+            </TabList>
           </div>
+
+          <TabPanels class="aspect-h-1 aspect-w-1 w-full">
+            <TabPanel v-for="image in product.images" :key="image.id" class="h-[500px]">
+              <img :src="image.src" :alt="image.alt" class="h-full object-center sm:rounded-lg" />
+            </TabPanel>
+
+          </TabPanels>
+        </TabGroup>
+
+        <!-- Product info -->
+        <div class="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0 rtl">
+          <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ product.name }}</h1>
+
+          <div class="mt-3">
+            <h2 class="sr-only">Product information</h2>
+            <p class="text-3xl tracking-tight text-gray-900">{{ product.price }}</p>
+          </div>
+
+          <!-- Reviews -->
+          <div class="mt-3">
+            <h3 class="sr-only">Reviews</h3>
+            <div class="flex items-center">
+              <div class="flex items-center">
+                <StarIcon v-for="rating in [0, 1, 2, 3, 4]" :key="rating"
+                  :class="[product.rating > rating ? 'text-indigo-500' : 'text-gray-300', 'h-5 w-5 flex-shrink-0']"
+                  aria-hidden="true" />
+              </div>
+              <p class="sr-only">{{ product.rating }} out of 5 stars</p>
+            </div>
+          </div>
+
+          <div class="mt-6">
+            <h3 class="sr-only">Description</h3>
+
+            <div class="space-y-6 text-base text-gray-700" v-html="product.description" />
+          </div>
+
+          <form class="mt-6">
+            <!-- Colors -->
+            <div class="ltr flex items-end flex-col">
+              <h3 class="text-sm text-gray-600 rtl">رنگ ها </h3>
+
+              <RadioGroup v-model="selectedColor" class="mt-2">
+                <RadioGroupLabel class="sr-only">Choose a color</RadioGroupLabel>
+                <span class="flex items-center space-x-3">
+                  <RadioGroupOption as="template" v-for="color in product.colors" :key="color.name" :value="color"
+                    v-slot="{ active, checked }">
+                    <div
+                      :class="[color.selectedColor, active && checked ? 'ring ring-offset-1' : '', !active && checked ? 'ring-2' : '', 'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none']">
+                      <RadioGroupLabel as="span" class="sr-only">{{ color.name }}</RadioGroupLabel>
+                      <span aria-hidden="true"
+                        :class="[color.bgColor, 'h-8 w-8 rounded-full border border-black border-opacity-10']" />
+                    </div>
+                  </RadioGroupOption>
+                </span>
+              </RadioGroup>
+            </div>
+
+            <div class="sm:flex-col1 mt-10 flex">
+              <button type="submit"
+                class="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full">اضافه
+                کردن به سبد خرید</button>
+
+              <button type="button"
+                class="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
+                <HeartIcon class="h-6 w-6 flex-shrink-0" aria-hidden="true" />
+                <span class="sr-only">اضافه کردن به علاقه کندی ها</span>
+              </button>
+            </div>
+          </form>
+
+          <section aria-labelledby="details-heading" class="mt-12">
+            <h2 id="details-heading" class="sr-only">Additional details</h2>
+
+            <div class="divide-y divide-gray-200 border-t">
+              <Disclosure as="div" v-for="detail in product.details" :key="detail.name" v-slot="{ open }">
+                <h3>
+                  <DisclosureButton class="group relative flex w-full items-center justify-between py-6 text-left">
+                    <span :class="[open ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-medium']">{{ detail.name
+                    }}</span>
+                    <span class="ml-6 flex items-center">
+                      <PlusIcon v-if="!open" class="block h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                        aria-hidden="true" />
+                      <MinusIcon v-else class="block h-6 w-6 text-indigo-400 group-hover:text-indigo-500"
+                        aria-hidden="true" />
+                    </span>
+                  </DisclosureButton>
+                </h3>
+                <DisclosurePanel as="div" class="prose prose-sm pb-6">
+                 
+                  <div>
+                    <div class="px-4 sm:px-0">
+                  
+                    </div>
+                    <div class="mt-6 border-t border-gray-100">
+                      <dl class="divide-y divide-gray-100">
+                        <div v-for="item in detail.items" :key="item" class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                          <dt class="text-sm font-medium leading-6 text-gray-900">{{ item[0] }}</dt>
+                          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{item[1]}}</dd>
+                        </div>
+                    
+                      </dl>
+                    </div>
+                  </div>
+                </DisclosurePanel>
+              </Disclosure>
+            </div>
+          </section>
         </div>
       </div>
+    
+
+
+      <section class="bg-white px-6 py-24 sm:py-32 lg:px-8" >
+        <div class=" font-bold text-right text-3xl pb-10">
+          نظرات
+        </div>
+        <figure class="mx-auto rtl  border-t pb-10" v-for="item in 3">
+          <p class="sr-only">5 out of 5 stars</p>
+          <div class="flex gap-x-1 text-indigo-600">
+           
+          </div>
+          <blockquote class="mt-10 text-lg leading-8 tracking-tight text-gray-900  sm:leading-9">
+            <p>“Qui dolor enim consectetur do et non ex amet culpa sint in ea non dolore. Enim minim magna anim id minim eu cillum sunt dolore aliquip. Amet elit laborum culpa irure incididunt adipisicing culpa amet officia exercitation. Eu non aute velit id velit Lorem elit anim pariatur.”</p>
+          </blockquote>
+          <figcaption class="mt-10 flex items-center gap-x-6">
+            <img class="h-12 w-12 rounded-full bg-gray-200"  alt="" />
+            <div class="text-sm leading-6">
+              <div class="font-semibold text-gray-900">Judith Black</div>
+              <div class="mt-0.5 text-gray-600">CEO of Workcation</div>
+            </div>
+          </figcaption>
+        </figure>
+        <div class="flex items-start space-x-4 mt-10">
+          <div class="flex-shrink-0">
+            <img class="inline-block h-10 w-10 rounded-full bg-gray-200"
+             
+              alt="" />
+          </div>
+          <div class="min-w-0 flex-1">
+            <form action="#" class="relative">
+              <div
+                class="overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+                <label for="comment" class="sr-only ">Add your comment</label>
+                <textarea rows="3" name="comment" id="comment"
+                  class="block w-full p-3  resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="Add your comment..." />
+  
+                <!-- Spacer element to match the height of the toolbar -->
+                <div class="py-2" aria-hidden="true">
+                  <!-- Matches height of button in toolbar (1px border + 36px content height) -->
+                  <div class="py-px">
+                    <div class="h-9" />
+                  </div>
+                </div>
+              </div>
+  
+              <div class="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
+                <div class="flex items-center space-x-5">
+  
+                </div>
+                <div class="flex-shrink-0">
+                  <button type="submit"
+                    class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Post</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
-  </template>
+
+  </div>
+</template>
   
-  <script setup>
-  import { ref } from 'vue'
-  import {
-    Disclosure,
-    DisclosureButton,
-    DisclosurePanel,
-    RadioGroup,
-    RadioGroupLabel,
-    RadioGroupOption,
-    Tab,
-    TabGroup,
-    TabList,
-    TabPanel,
-    TabPanels,
-  } from '@headlessui/vue'
-  import { StarIcon } from '@heroicons/vue/20/solid'
-  import { HeartIcon, MinusIcon, PlusIcon } from '@heroicons/vue/24/outline'
-  
-  const product = {
-    name: 'ایفون ۱۴',
-    price: '۱۴۰۰۰۰۰۰ تومان',
-    rating: 4,
-    images: [
-      {
-        id: 1,
-        name: 'Angled view',
-        src: '/images/5.jpg',
-        alt: 'Angled front view with bag zipped and handles upright.',
-      },
-      {
-        id: 1,
-        name: 'Angled view',
-        src: '/images/3.jpg',
-        alt: 'Angled front view with bag zipped and handles upright.',
-      },
-      {
-        id: 1,
-        name: 'Angled view',
-        src: '/images/4.jpg',
-        alt: 'Angled front view with bag zipped and handles upright.',
-      },
-      // More images...
-    ],
-    colors: [
-      { name: 'Washed Black', bgColor: 'bg-gray-700', selectedColor: 'ring-gray-700' },
-      { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400' },
-      { name: 'Washed Gray', bgColor: 'bg-gray-500', selectedColor: 'ring-gray-500' },
-    ],
-    description: `
+<script setup>
+import { ref } from 'vue'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  RadioGroup,
+  RadioGroupLabel,
+  RadioGroupOption,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from '@headlessui/vue'
+import { StarIcon } from '@heroicons/vue/20/solid'
+import { HeartIcon, MinusIcon, PlusIcon } from '@heroicons/vue/24/outline'
+
+const product = {
+  name: 'ایفون ۱۴',
+  price: '۱۴۰۰۰۰۰۰ تومان',
+  rating: 4,
+  images: [
+    {
+      id: 1,
+      name: 'Angled view',
+      src: '/images/5.jpg',
+      alt: 'Angled front view with bag zipped and handles upright.',
+    },
+    {
+      id: 1,
+      name: 'Angled view',
+      src: '/images/3.jpg',
+      alt: 'Angled front view with bag zipped and handles upright.',
+    },
+    {
+      id: 1,
+      name: 'Angled view',
+      src: '/images/4.jpg',
+      alt: 'Angled front view with bag zipped and handles upright.',
+    },
+    // More images...
+  ],
+  colors: [
+    { name: 'Washed Black', bgColor: 'bg-gray-700', selectedColor: 'ring-gray-700' },
+    { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400' },
+    { name: 'Washed Gray', bgColor: 'bg-gray-500', selectedColor: 'ring-gray-500' },
+  ],
+  description: `
       <p>امکان برگشت کالا در گروه موبایل با دلیل "انصراف از خرید" تنها در صورتی مورد قبول است که پلمب کالا باز نشده باشد.
  تمام گوشی‌های دیجی‌کالا ضمانت رجیستری دارند. در صورت وجود مشکل رجیستری، می‌توانید بعد از مهلت قانونی ۳۰ روزه، گوشی خریداری‌شده را مرجوع کنید.</p>
     `,
-    details: [
-      {
-        name: 'امکانات',
-        items: [
-          'فناوری صفحه‌نمایش : Super Retina XDR OLED',
-          'اندازه : 6.1',
-          'رزولوشن عکس :12 مگاپیکسل',
-          'نسخه سیستم عامل : iOS 15',
-          'اقلام همراه :دفترچه‌ راهنما، کابل Lightning',
+  details: [
+    {
+      name: 'امکانات',
+      items: [
+        ['فناوری صفحه‌نمایش ','Super Retina XDR OLED'],
+        ['اندازه' ,' 6.1'],
+        ['رزولوشن عکس' ,'12 مگاپیکسل'],
+        ['نسخه سیستم عامل ','iOS 15'],
+        ['اقلام همراه' ,'دفترچه‌ راهنما، کابل Lightning'],
 
-        ],
-      },
-      // More sections...
-    ],
-  }
-  
-  const selectedColor = ref(product.colors[0])
-  </script>
+      ],
+    },
+    // More sections...
+  ],
+}
+
+const selectedColor = ref(product.colors[0])
+</script>
