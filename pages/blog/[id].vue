c6c9197e894431b8a3ca6086082de55645af5a48
sinaazadfar <sinaazadfar@gmail.com>
2024-02-20 18:02:32 +0330

    sina

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="w-full h-full">
+    {{ blog }}
+    <div v-if="blog != null" class="w-full h-full">
         <div class="w-full h-56 bg-indigo-600 flex py-10 flex-col shadow-xl justify-center items-center space-y-4" />
         <div class=" shadow-lg -mt-32 pb-10 flex justify-center">
             <div class="max-w-[65rem] flex flex-col shadow-lg rounded-[25px] overflow-hidden">
@@ -7,12 +8,12 @@
                 
                <div class="grid grid-cols-4 gap-4 bg-white p-4 pb-10">
                      <div class="col-span-3  ">
-                         <div class="relative isolate h-[30rem]">
+                         <div class="relative isolate shadow-xl rounded-[25px] pb-10 h-[30rem]">
                              
                              <img :src="blog.imageBlog.photo" :alt="blog.imageBlog.title_for_photo"
                                  class="absolute inset-0 -z-1000  h-[30rem] w-full rounded-[25px] object-cover" />
                                  <div
-                                 class="absolute inset-0 -z-9999 bg-gradient-to-r from-gray-500 to-gray-900 opacity-80 rounded-[25px] h-[30rem] w-full" />
+                                 class="absolute inset-0 -z-9999 bg-gradient-to-tr from-gray-700 to-gray-900 opacity-80 rounded-[25px] h-[30rem] w-full" />
                            
                              <div class="absolute inset-0 -z-1000  h-[30rem] w-full ">
                                  <h1 class="font-bold rtl px-16 pt-16 text-white text-5xl">
@@ -56,27 +57,7 @@ export default {
 
     },
     data: () => ({
-        blog: {
-            "title": "این یک عنوان تست است",
-            "body": "<p>dfb</p>",
-            "file": [],
-            "imageBlog": {
-                "id": 4,
-                "title_for_photo": "dfb",
-                "photo": "/images/6.jpg"
-            },
-            "id": 3,
-            "author": {
-                "username": "alishams",
-                "get_full_name": "ali shams",
-                "image": null
-            },
-            "like_count": 0,
-            "likeAuthor": false,
-            "category": [],
-            "created_at": "1402/10/29",
-            "updated_at": "1402/10/29"
-        },
+        blog: null,
     }),
     methods: {
         getData() {
