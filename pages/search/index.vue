b1018dfa8c2fb093d2fc5f3ab9bccdb8341c2d2b
sinaazadfar <sinaazadfar@gmail.com>
2024-02-26 12:30:25 +0330

    sina

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,34 +1,43 @@
 
 <template>
+  <div>
+      <div class="  relative ">
+
+
+      <div class="bg rounded-b-[50px]"></div>
+<div class="bg bg2 rounded-b-[50px]"></div>
+<div class="bg bg3 rounded-b-[50px]"></div>
+
+
+
+      <img src="/images/header.avif" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover " />
+      <div class="absolute inset-x-0 -top-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+        <div
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+      </div>
+    
+
+      <div
+        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true">
+        <div
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+      </div>
+    </div>
+  </div>
   <div class="min-h-full">
-    <Popover as="header" class="bg-indigo-600 pb-24" v-slot="{ open }">
+    
+    <Popover as="header" class=" pb-24" v-slot="{ open }">
+    
       <div class="mx-auto max-w-3xl px-4 py-8 sm:px-6 lg:max-w-7xl lg:px-8">
-        <!-- Search -->
-        <!-- <div class="relative w-full flex justify-center">
-          <div class="relative w-1/2  ">
-            <div class="flex p-2 bg-indigo-700 rounded-[28px] ">
-              <div class=" flex-auto">
-                <input type="search" id="search-dropdown" v-model="text" class=" block p-4 w-full z-20 text-sm text-indigo-500 text-right rtl pr-5
-                                 rounded-sm rounded-e-[20px]  " placeholder="جستجو بین محصولات  هزاران فروشگاه ..."
-                  required>
-              </div>
-              <div class=" pl-1">
-                <button type="submit"
-                  class=" pl-9 pr-14 top-0 end-0 h-full p-4 text-sm w-10 font-medium  text-white bg-orange-600 rounded-sm rounded-e-[20px]">
-                  <svg class="w-4 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                  </svg>
-                  <span class="sr-only">Search</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div> -->
+     
+        
+    
         <div class="flex justify-center">
             <div class="relative w-[50rem]">
-              <input type="search" id="search-dropdown"  v-on:keyup.enter="$router.push('/search/?search='+text);" v-model="searchQuery" @input="filterOptions"
+              <input type="search" id="search-dropdown" v-model="text" 
                 class=" block p-4 w-full z-20 text-sm text-indigo-900 bg-white text-right rtl pr-20  rounded-[20px] "
                 placeholder="جستجو بین محصولات  هزاران فروشگاه ..." required>
               <button type="submit"
