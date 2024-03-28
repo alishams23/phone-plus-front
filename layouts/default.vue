dafc2c3d562dd793cf7567515f0d2962bdd3ac91
sinaazadfar <sinaazadfar@gmail.com>
2024-03-28 16:27:24 +0330

    ss

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,29 @@
 
 <template>
-  
-  
-      <Navbar   />
-  
- 
-        <slot />  
-   
-        <Footer />
-   
-  </template>
-  <script>
-import Navbar from "@/components/section/Navigation.vue"
- import Footer from "@/components/section/Footer.vue"
-export default {
-  components:{
-    Navbar,
-    Footer
-  },
-  data: () => ({
-    
-  }),
-}
+
+  <Navbar :buttons="buttons" />
+
+  <slot />  
+
+  <Footer />
+
+</template>
+
+<script>
+  import Navbar from "@/components/section/Navigation.vue"
+  import Footer from "@/components/section/Footer.vue"
+
+  export default {
+    components:{
+      Navbar,
+      Footer
+    },
+    data: () => ({
+      buttons:[{
+        'name':'محبوب ترین محصولات',
+        'func':null,
+        'href':'/search/?sort_product=-rate',
+      }]
+    }),
+  }
 </script>
