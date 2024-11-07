db672e64ce097e30487e99ead6f8320ce98e3544
sinaazadfar <sinaazadfar@gmail.com>
2024-11-07 20:48:24 +0330

    sina

--- pages/p/dashboard/digitalProductOrders.vue
+++ pages/p/dashboard/digitalProductOrders.vue
@@ -68,27 +68,29 @@
                                 </div>
                             </div>
                             </div>
-                            <div v-if="lastOrder.digital_product.file" class="flex justify-center pe-4">
-                            <a
-                                :href="lastOrder.digital_product.file"
-                                class="inline-flex items-center justify-center px-5 mx-5 py-3 mb-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                                </svg>
-                                دانلود فایل
-                            </a>
-                            </div>
-                            <div v-if="lastOrder.digital_product.link_file" class="flex justify-center pe-4">
-                            <a
-                                :href="lastOrder.digital_product.link_file"
-                                class="inline-flex items-center justify-center px-5 py-3 mb-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                                </svg>
-                                دانلود فایل
-                            </a>
+                            <div class="flex justify-center pe-4" >
+                                <div v-if="lastOrder.digital_product.file!=null || lastOrder.digital_product.file!='null'" class="flex justify-center pe-4">
+                                <a
+                                    :href="lastOrder.digital_product.file"
+                                    class="inline-flex items-center justify-center px-5 mx-5 py-3 mb-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                                    </svg>
+                                    دانلود فایل
+                                </a>
+                                </div>
+                                <div v-if="lastOrder.digital_product.link_file!=null || lastOrder.digital_product.link_file!='null'" class="flex justify-center pe-4">
+                                <a
+                                    :href="lastOrder.digital_product.link_file"
+                                    class="inline-flex items-center justify-center px-5 py-3 mb-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                                    </svg>
+                                    دانلود فایل
+                                </a>
+                                </div>
                             </div>
                             
                             <div class="text-right pe-4 pt-2" >
@@ -101,21 +103,21 @@
                                     <p class="pe-1 text-[10px] text-gray-400 rtl">کدپیگیری : </p>
                                 </div>
                             </div>
-                            <div class="mt-3 text-right text-[10px] ps-4 text-gray-400 rtl">
+                            <div class="mt-3 text-right text-[10px] ps-4 text-gray-400 pb-3 rtl">
                                 <p>
                                     {{ lastOrder.jalali_time }}
                                 </p>
                             </div>
-                            <div v-if="lastOrder.digital_product.instructions" class="flex justify-end pe-4 mt-4">
+                            <div v-if="lastOrder.digital_product.instructions!='null'" class="flex justify-end pe-4 mt-4">
                                 <div @click="toggleInstructions(lastOrder.id)" 
-                                    class="inline-flex items-center justify-center px-5 py-3 mb-3 border border-transparent text-base font-medium rounded-md text-indigo-600 cursor-pointer hover:text-indigo-700">
+                                    class="inline-flex items-center justify-center px-5 pb-3 mb-3 border border-transparent text-base font-medium rounded-md text-indigo-600 cursor-pointer hover:text-indigo-700">
                                     نکات ضروری برای استفاده
                                 </div>
                             </div>
                         </div>
                         </div>
                             <transition name="fade">
-                                <div v-if="!showInstructionsId.includes(lastOrder.id)" class=" my-4 w-full mx-2 lg:mx-4 px-4 py-2 rtl bg-gray-200 rounded-lg">
+                                <div v-if="!showInstructionsId.includes(lastOrder.id) && lastOrder.digital_product.instructions!='null'" class=" my-4 w-full mx-2 lg:mx-4 px-4 py-2 rtl bg-gray-200 rounded-lg">
                                     <div v-html="lastOrder.digital_product.instructions"></div>
                                 </div>
                             </transition>
