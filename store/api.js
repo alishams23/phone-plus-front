08a60391ddd88e9b7d6e47634631c7e0b717e1d4
ali <alishams.s1380@gmail.com>
2025-01-20 01:27:02 +0330

    change main urls

--- store/api.js
+++ store/api.js
@@ -4,9 +4,9 @@ import { defineStore } from 'pinia';
 
 export const apiStore = defineStore('api', {
   state: () => ({
-    address: 'https://new.phoneplus.ir',
+    address: 'https://phoneplus.ir',
     // address: 'http://127.0.0.1:8000',
-    ws: 'wss://new.phoneplus.ir',
+    ws: 'wss://phoneplus.ir',
   }),
   actions: {
 
