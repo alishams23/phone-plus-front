3818e2ef40058106d02cc4d4d5f86eb42e2a17c9
sinaazadfar <sinaazadfar@gmail.com>
2025-04-23 19:39:02 +0330

    fix

--- components/section/LoginPopup.vue
+++ components/section/LoginPopup.vue
@@ -24,7 +24,7 @@
                             </h2>
                         </div>
                         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                            <form  @submit.prevent="sendLoginSms" @keydown.enter="sendLoginSms" class="space-y-6">
+                            <form  @submit.prevent="sendLoginSms" class="space-y-6">
                                 <div class="ltr">
                                     <label for="phone-number"
                                         class="block text-sm  font-medium leading-6 text-gray-900">شماره
@@ -85,7 +85,7 @@
                                 کد را وارد کنید</h2>
                         </div>
                         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                            <form  @submit.prevent="check_code" @keydown.enter="check_code" class="space-y-6">
+                            <form  @submit.prevent="check_code" class="space-y-6">
                                 <div class="ltr">
                                     <div class="relative mt-2 rounded-full shadow-sm">
                                         <input type="text" placeholder="کد پیامک شده" v-model="code"
@@ -148,7 +148,7 @@
                                 ثبت نام کنید</h2>
                         </div>
                         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                            <form  @submit.prevent="sendSingUpSms" @keydown.enter="sendSingUpSms" class="space-y-6">
+                            <form  @submit.prevent="sendSingUpSms" class="space-y-6">
                                 <div class="sm:col-span-3">
                                     <label for="first-name"
                                         class="block text-sm font-medium leading-6 text-gray-900">نام</label>
@@ -192,7 +192,6 @@
                                     </div>
                                 </div>
                                 <div>
-                                    <!-- <div @click="sendSingUpSms" class="flex w-full justify-center rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">دریافت کد</div> -->
                                     <div
                                         class="flex w-full justify-center rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
 
@@ -238,7 +237,7 @@
                                 کد را وارد کنید</h2>
                         </div>
                         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                            <form  @submit.prevent="check_code_signup" @keydown.enter="check_code_signup" class="space-y-6">
+                            <form @submit.prevent="check_code_signup" class="space-y-6">
                                 <div class="ltr">
                                     <div class="relative mt-2 rounded-full shadow-sm">
                                         <input type="text" placeholder="کد پیامک شده" v-model="code"
@@ -250,11 +249,9 @@
                                     </div>
                                 </div>
                                 <div>
-
                                     <div
                                         class="flex w-full justify-center rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                        <button @click="check_code_signup" class="w-full"
-                                            v-if="loading == false">
+                                        <button type="submit" class="w-full" v-if="loading == false">
                                             تایید
                                         </button>
                                         <div v-if="loading">
@@ -269,9 +266,7 @@
                                                     d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
                                                     fill="currentColor" />
                                             </svg>
-
                                         </div>
-
                                     </div>
                                     <div v-if="isCooldownActive"
                                         class="text-indigo-500 flex  justify-center pt-5">
